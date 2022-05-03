96355c696e5f01ec198aa25b351487b72cd3378b
JeffiProgrammer <fx.ranjbaran@gmail.com>
2022-05-03 16:16:47 +0430

    Basics of project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quize_application/home_page.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "dana"),
+      home: Scaffold(
+        body: HomeWidget(),
+      ),
+    );
+  }
+}
