b477d7efbd0ad37ec245d4644bacde17d78ccb2f
JeffiProgrammer <fx.ranjbaran@gmail.com>
2022-05-15 15:46:45 +0430

    Update quiz page

--- lib/quize_page.dart
+++ lib/quize_page.dart
@@ -16,7 +16,53 @@ class QuizePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: SafeArea(
-        child: Text('QuizzzzzPage'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+            ),
+            Image(
+              image: AssetImage('images/1.png'),
+              height: 200.0,
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text(
+              'مشهورترین شعبده باز دنیا کیست؟',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 17.0),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ اول',
+                textAlign: TextAlign.end,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ دوم',
+                textAlign: TextAlign.end,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ سوم',
+                textAlign: TextAlign.end,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'پاسخ چهارم',
+                textAlign: TextAlign.end,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
